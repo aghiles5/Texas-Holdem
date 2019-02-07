3a81f87cb195ee2cc129ad3d8de9b1f01aa8ed61
jzlowie <47369757+jzlowie@users.noreply.github.com>
2019-02-07 10:57:07 -0700

    Update AI.java

--- AI.java
+++ AI.java
@@ -1,3 +1,18 @@
 public class AI {
+  /// Might change private int to public int
+  
+  private int card_1;
+  private int card_2;
+  
+  public int getCards() {
+    
+    return card_1;
+    return card_2;
+  
+  }
+  
+  /*
+  Might implement random AI decisions here but we will talk about it next meeting
+  */
 
 }
