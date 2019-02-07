04ac7d4f696ef998c41b1208f3197e2552a73a89
jzlowie <47369757+jzlowie@users.noreply.github.com>
2019-02-07 10:54:13 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,15 +1,22 @@
 
 public class Player {
-  ///Might change private to public
+  /// Might change private int to public int
   
   private int card_1;
   private int card_2;
   
+  /// Accessot for player's card
   public int getCards() {
     
     return card_1;
     return card_2;
     
   }
+  
+  /*
+  
+  Might possibly implement player decision methods into this class but we can talk about it next meeting
+  
+  */
 
 }
