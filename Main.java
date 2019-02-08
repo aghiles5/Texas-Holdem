861055910e1bf6ce889274dfc5c44b68ca73eac0
jzlowie <47369757+jzlowie@users.noreply.github.com>
2019-02-08 14:02:04 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,14 @@
+/* This line is required as it is used to get user input
+ * for player action during game
+ */
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("There is no game lmao")
 
 	}
 
