929b1b563379ef3f0ef92b58f99b028dc0e5d752
John Lowie <jzlowie@yahoo.com>
2019-02-12 15:19:45 -0700

    Update SuperPlayer.java

--- SuperPlayer.java
+++ SuperPlayer.java
@@ -1,5 +1,12 @@
 // Common methods between AI and Player class will be in this super class
 
 public class SuperPlayer {
-    
+    /* implements
+    check
+    fold
+    raise
+    call
+    and bet
+    methods
+    */
 }
