f17781cf40d362bafca1045fbc8b2c267fb2fe23
Kiiii01 <47369737+Kiiii01@users.noreply.github.com>
2019-02-19 15:53:42 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,11 +1,9 @@
 import java.util.ArrayList;
 
-public class Player {
+public class Player extends SuperPlayer{
   private int card_1;
   private int card_2;
-  private int Money; //not used until BetRaise is done
-  private ArrayList<Integer> hand = new ArrayList<Integer>();
-  
+
   Player() {
 	 //Use deck class to set hand
   }
@@ -22,37 +20,5 @@ public class Player {
     return hand;
     
   }
-  
-  public void check(String choice) {
-	  //If the input is c, nothing happens
-	  if (choice.equalsIgnoreCase("C")) {
-		  System.out.println("Player Checked.");
-	  }
-  }
-  
-  public void fold(String choice) {
-	  //if the input is f, the list is emptied
-	  if (choice.equalsIgnoreCase("F")) {
-		  hand.clear(); //list is emptied
-		  System.out.println("Player folded.");
-	  }
-  }
-  
-  public void BetRaise(String choice, int bet) {
-	  if (choice.equalsIgnoreCase("B")) {
-		  //checks to see if the bet is less than the
-		  //money in the player's balance
-		  if (bet <= Money) {
-			  Money = Money - bet;
-			  //pot update
-		  }
-	  } else if (choice.equalsIgnoreCase("R")) {
-		  //Raise action
-		  //if bet >= 2 times pot && bet <= Money
-		  Money = Money - bet;
-		  //pot update
-	  }
-  }
-
 
 }
