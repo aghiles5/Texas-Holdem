6968d08e28db99d2bdc7de6ac4265baa5ac6595e
John Lowie <jzlowie@yahoo.com>
2019-02-20 14:42:35 -0700

    Update AI.java

--- AI.java
+++ AI.java
@@ -25,10 +25,16 @@ public class AI extends Player{
     }
 
     else if (decision == 2) {
+      // update this call of method
+      // money argument needs updating
+      checkAIBets(decision, money);
       // super.BetRaise("B", bet);
     }
 
     else if (decision == 3) {
+      // update this call of method
+      // money argument needs updating
+      checkAIBets(decision, money);
       // super.BetRaise("R", bet);
     }
 
@@ -44,10 +50,28 @@ public class AI extends Player{
   }
 
   // This method will randomly bet and check if AI has sufficient money
-  // This method will eventually take an argument
-  public int checkAIBets(/* money */) {
+  // money argument must ge retrieved from somewhere
+  public int checkAIBets(int playDecision, int money) {
     Random bet = new Random();
-    int betting = bet.nextInt(/* money */);
+    // int betting = bet.nextInt(money + 1);
+
+    if (playDecision == 2) {
+      int betting = bet.nextInt(money + 1);
+      
+      if (betting == 0) {
+        // must retrieve money
+        // money argument is not working
+        checkAIBets(playDecision, money);
+      }
+
+      else if (betting == money) {
+        checkAIBets(playDecision, money);
+      }
+
+      else {
+        return 0;
+      }
+    }
 
     return 0;
   }
