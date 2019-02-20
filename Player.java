81462eade69e25e5618528f15f76a847c722fb45
Kyle Wen <Kyle Wen@10.12.142.45>
2019-02-20 13:54:37 -0700

    Card fix

--- Player.java
+++ Player.java
@@ -1,14 +1,14 @@
 import java.util.ArrayList;
 
 public class Player extends SuperPlayer{
-  private int card_1;
-  private int card_2;
+  private Card card_1;
+  private Card card_2;
 
   Player() {
 	 //Use deck class to set hand
   }
   
-  public ArrayList<Integer> setCards() {
+  public ArrayList<Card> setCards() {
 	  //adds the cards to the hand list
 	  hand.add(card_1);
 	  hand.add(card_2);
@@ -16,7 +16,7 @@ public class Player extends SuperPlayer{
   }
   
   /// Accessor for player's card
-  public ArrayList<Integer> getCards() {
+  public ArrayList<Card> getCards() {
     return hand;
     
   }
