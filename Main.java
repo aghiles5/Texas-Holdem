654963cd579e920ca736e9b7732bc0d55fe30b2c
BSchmaltzC <47369750+BSchmaltzC@users.noreply.github.com>
2019-02-26 12:12:30 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,6 +5,9 @@
  * @author Brayden
  * @version 02/20/18
  */
+
+ //Must change all the player1, player2 things to loops later on when using AI and more players
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -12,11 +15,13 @@ public class Main {
 	private boolean roundOccur;
 	private boolean gameOccur;
 	private Deck cardDeck = new Deck();
-	private Player player1 = new Human();
-	private Player player2 = new Human();
+	private Player player1 = new Human("Player1");
+	private Player player2 = new Human("Player2");
 	private ArrayList<Player> players = new ArrayList<Player>();
 	private ArrayList<Card> middleCards = new ArrayList<Card>();
+	private ArrayList<Card> playerHole = new ArrayList<Card>();
 	private int playerCount;
+	private int aNewGame;
 
 	public Main(){
 		roundOccur = true;
@@ -24,9 +29,26 @@ public class Main {
 		players.add(player1);
 		players.add(player2);
 		playerCount = 0;
+		aNewGame = 0;
 	}
 
-	private void setupRound(){
+	private String findName(){
+		String name = "";
+		System.out.printf("Please enter your name player: ");
+		Scanner nameInput = new Scanner(System.in);
+		name = nameInput.next();
+		return name;
+	}
+
+	private void newGame(){
+		players.clear();
+		player1 = null;
+		player2 = null;
+		player1 = new Human(findName());
+		player2 = new Human(findName());
+		players.add(player1);
+		players.add(player2);
+		playerCount = 0;
 		cardDeck = null;
 		cardDeck = new Deck();
 		//Change this to for loop later on for more players
@@ -34,6 +56,28 @@ public class Main {
 		player2.setHole(cardDeck.dealSingle());
 		player1.setHole(cardDeck.dealSingle());
 		player2.setHole(cardDeck.dealSingle());
+	}
+
+	private void setupRound(){
+		player1.emptyHole();
+		player1.emptyHand();
+		player2.emptyHole();
+		player2.emptyHand();
+		cardDeck = null;
+		cardDeck = new Deck();
+		//Change this to for loop later on for more players
+		player1.setHole(cardDeck.dealSingle());
+		player2.setHole(cardDeck.dealSingle());
+		player1.setHole(cardDeck.dealSingle());
+		player2.setHole(cardDeck.dealSingle());
+	}
+
+	private String getPlayerInput(){
+		String decision = "";
+		System.out.printf("What would you like to do next (C for Check, F for Fold): ");
+		Scanner dcInput = new Scanner(System.in);
+		decision = dcInput.next();
+		return decision;
 	}
 
 	public void startGame() {
@@ -45,32 +89,44 @@ public class Main {
 				Scanner paInput = new Scanner(System.in);
 				playAgain = paInput.next();
 
-				while(!playAgain.equals("Y") || !playAgain.equals("N")){
+				while(!playAgain.equalsIgnoreCase("Y") || !playAgain.equalsIgnoreCase("N")){
 					System.out.printf("Enter Y or N to have a valid input: ");
 					playAgain = paInput.next();
 				}
 
-				if(playAgain == "N"){
+				if(playAgain.equalsIgnoreCase("N")){
 					break;
 				}
+
+				else{
+					newGame();
+				}
+
+			}
+
+			else if(aNewGame == 0){
+				newGame();
+				aNewGame = 1;
 			}
 
 			else{
 				String quitGame = "";
-				System.out.printf("If you would like to quit the game please input Q: ");
+				System.out.printf("Please input Q to Quit Game or Anything else to Continue: ");
 				Scanner quitInput = new Scanner(System.in);
 				quitGame = quitInput.next();
 
 				if(quitGame.equalsIgnoreCase("Q")){
 					break;
 				}
-			}
 
-			setupRound();
+				else{
+					setupRound();
+				}
+			}
 
 			//Allows for the round to keep going
 			while(roundOccur){
-				
+				for(int i = 0; i)
 			}
 		}
 	}
