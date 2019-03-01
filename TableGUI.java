e2de0a094c0266b5716da54a3f9e36ee28544806
aghiles5 <47369723+aghiles5@users.noreply.github.com>
2019-03-01 10:43:47 -0700

    Circular panes, use jfxtras library in resources

--- TableGUI.java
+++ TableGUI.java
@@ -5,6 +5,7 @@ import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+import jfxtras.scene.layout.CircularPane;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -36,13 +37,14 @@ public class TableGUI extends Application {
 		
 		//Left bar
 		
+		/*
 		VBox comBar = new VBox();
 		comBar.setMinSize(windowW * (7.0 / 38.0), windowH);
-		comBar.setSpacing(20);
+		comBar.setSpacing(10);
 		comBar.setAlignment(Pos.CENTER);
 		comBar.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;"
-		        + "-fx-border-width: 1;" + "-fx-border-insets: 5;"
-		        + "-fx-border-color: black;");
+		        + "-fx-border-width: 5;" + "-fx-border-color: maroon;" 
+				+ "-fx-background-color: indianred;");
 		for (int index = 1; index < players.size(); index++) {
 			VBox comEntry = new VBox();
 			
@@ -62,26 +64,43 @@ public class TableGUI extends Application {
 			comEntry.getChildren().addAll(name, stack, bet, action, cards);
 			comBar.getChildren().add(comEntry);
 		}
+		*/
 		
 		//Right bar
 		
 		VBox playerBar = new VBox();
-		playerBar.setMinSize(windowW * (7.0 / 38.0), windowH);
+		playerBar.setMinSize(windowW * (1.0 / 4.0), windowH);
 		playerBar.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;"
 		        + "-fx-border-width: 1;" + "-fx-border-insets: 5;"
 		        + "-fx-border-color: black;");
 		
 		//Centre table
 		
-		GridPane table = new GridPane();
-		table.setMinSize(windowW * (9.0 / 16.0), windowH);
-		table.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;"
-		        + "-fx-border-width: 1;" + "-fx-border-insets: 5;"
-		        + "-fx-border-color: black;");
+		CircularPane table = new CircularPane();
+		table.setMinSize(windowW * (3.0 / 4.0), windowH);
+		for (Player player : players) {
+			VBox comEntry = new VBox();
+			
+			Label name = new Label(player.getName());
+			Text stack = new Text("Stack: ");
+			Text bet = new Text("Bet: ");
+			Text action = new Text("Action: ");
+			
+			HBox cards = new HBox();
+			cards.setSpacing(10);
+			cards.setAlignment(Pos.CENTER);
+			Image card1 = new Image("/Images/TempCard.png");
+			Image card2 = new Image("/Images/TempCard.png");
+			cards.getChildren().addAll(new ImageView(card1), new ImageView(card2));
+			
+			comEntry.setAlignment(Pos.CENTER);
+			comEntry.getChildren().addAll(name, stack, bet, action, cards);
+			table.add(comEntry);
+		}
 		
 		root.setRight(playerBar);
 		root.setCenter(table);
-		root.setLeft(comBar);
+		//root.setLeft(comBar);
 		primaryStage.show();
 	}
 	
