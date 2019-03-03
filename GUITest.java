b1cccb58174d4ad36cf1ea914e809eb646a7cbbb
aghiles5 <47369723+aghiles5@users.noreply.github.com>
2019-03-02 21:31:21 -0700

    Layout on its way, trouble with circles

--- GUITest.java
+++ GUITest.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList;
 
 public class GUITest {
+	public static Deck testDeck = new Deck();
+	
 	public static void main(String args[]) {
-		Deck testDeck = new Deck();
-		testDeck.shuffle();
-		ArrayList<Card> comm = new ArrayList<Card>();
+	}
+	
+	public static ArrayList<Player> generatePlayers() {
 		ArrayList<Player> players = new ArrayList<Player>();
 		
 		players.add(new Human("User"));
@@ -13,17 +15,26 @@ public class GUITest {
 		players.add(new Human("COM 3"));
 		players.add(new Human("COM 4"));
 		players.add(new Human("COM 5"));
+		players.add(new Human("COM 6"));
+		players.add(new Human("COM 7"));
+		players.add(new Human("COM 8"));
+		players.add(new Human("COM 9"));
 		
 		for (int i = 0; i < 2; i++) {
 			for (Player player : players)
 				player.setHole(testDeck.dealSingle());
 		}
 		
+		return players;
+	}
+	
+	public static ArrayList<Card> generateComm() {
+		ArrayList<Card> comm = new ArrayList<Card>();
+		
 		comm = testDeck.dealCard(comm);
 		comm = testDeck.dealCard(comm);
 		comm = testDeck.dealCard(comm);
 		
-		TableGUI.passArrayLists(players, comm);
-		TableGUI.main(null);
+		return comm;
 	}
 }
--- TableGUI.java
+++ TableGUI.java
@@ -11,18 +11,28 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
 public class TableGUI extends Application {
-	private static ArrayList<Player> players = new ArrayList<Player>();
-	private static ArrayList<Card> comm = new ArrayList<Card>();
-	private static int pot;
-	private static int highestBet;
+	private final static double tableWidth = 720; //Referring to the length of the straightway
+	private final static double tableRatio = 1.25;
+	private final static double tableRim = 20;
+	private final static double playerOutset = 60; //The distance of player information from the table
+	private final static double playerInset = 20; //The distance of player bets and cards? inside the table
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception{
+		GUITest.testDeck.shuffle();
+		ArrayList<Card> comm = GUITest.generateComm();
+		ArrayList<Player> players = GUITest.generatePlayers();
+		
+		
 		BorderPane root = new BorderPane();
 		
 		Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
@@ -35,50 +45,18 @@ public class TableGUI extends Application {
 		primaryStage.setFullScreen(true);
 		primaryStage.setFullScreenExitHint("");
 		
-		//Left bar
+		//Action Bar
 		
-		/*
-		VBox comBar = new VBox();
-		comBar.setMinSize(windowW * (7.0 / 38.0), windowH);
-		comBar.setSpacing(10);
-		comBar.setAlignment(Pos.CENTER);
-		comBar.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;"
-		        + "-fx-border-width: 5;" + "-fx-border-color: maroon;" 
-				+ "-fx-background-color: indianred;");
-		for (int index = 1; index < players.size(); index++) {
-			VBox comEntry = new VBox();
-			
-			Label name = new Label(players.get(index).getName());
-			Text stack = new Text("Stack: ");
-			Text bet = new Text("Bet: ");
-			Text action = new Text("Action: ");
-			
-			HBox cards = new HBox();
-			cards.setSpacing(10);
-			cards.setAlignment(Pos.CENTER);
-			Image card1 = new Image("/Images/TempCard.png");
-			Image card2 = new Image("/Images/TempCard.png");
-			cards.getChildren().addAll(new ImageView(card1), new ImageView(card2));
-			
-			comEntry.setAlignment(Pos.CENTER);
-			comEntry.setSpacing(3);
-			comEntry.getChildren().addAll(name, stack, bet, action, cards);
-			comBar.getChildren().add(comEntry);
-		}
-		*/
+		HBox actionBar = new HBox();
+		actionBar.setPrefSize(windowW, windowH / 6.0);
+		actionBar.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;"
+		        + "-fx-border-width: 5;");
 		
-		//Right bar
+		//Table
 		
-		VBox playerBar = new VBox();
-		playerBar.setMinSize(windowW * (1.0 / 4.0), windowH);
-		playerBar.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;"
-		        + "-fx-border-width: 1;" + "-fx-border-insets: 5;"
-		        + "-fx-border-color: black;");
+		CircularPane playerRing = new CircularPane();
+		playerRing.setPrefSize(windowW * (5.0 / 6.0), windowH * (5.0 / 6.0));
 		
-		//Centre table
-		
-		CircularPane table = new CircularPane();
-		table.setMinSize(windowW * (3.0 / 4.0), windowH);
 		for (Player player : players) {
 			VBox comEntry = new VBox();
 			
@@ -90,35 +68,162 @@ public class TableGUI extends Application {
 			HBox cards = new HBox();
 			cards.setSpacing(10);
 			cards.setAlignment(Pos.CENTER);
-			Image card1 = new Image("/Images/TempCard.png");
-			Image card2 = new Image("/Images/TempCard.png");
+			Image card1 = new Image("/Images/" + player.getHole().get(0).getSuit() + "/" + player.getHole().get(0).getRank() + ".png");
+			Image card2 = new Image("/Images/" + player.getHole().get(1).getSuit() + "/" + player.getHole().get(1).getRank() + ".png");
 			cards.getChildren().addAll(new ImageView(card1), new ImageView(card2));
 			
 			comEntry.setAlignment(Pos.CENTER);
 			comEntry.getChildren().addAll(name, stack, bet, action, cards);
-			table.add(comEntry);
+			playerRing.add(comEntry);
 		}
 		
-		root.setRight(playerBar);
+		//Community cards
+		
+		VBox community = new VBox();
+		community.setAlignment(Pos.CENTER);	
+		community.setSpacing(5);
+		
+		HBox flop = new HBox();
+		flop.setAlignment(Pos.CENTER);	
+		flop.setSpacing(5);
+		for (int index = 0; index < 3; index++) {
+			Image cardImage = new Image("/Images/" + comm.get(index).getSuit() + "/" + comm.get(index).getRank() + ".png");
+			flop.getChildren().add(new ImageView(cardImage));
+		}
+		
+		HBox streets = new HBox();
+		streets.setAlignment(Pos.CENTER);	
+		streets.setSpacing(10);
+		for (int index = 3; index < 5; index++) {
+			Image cardImage = new Image("/Images/" + comm.get(index).getSuit() + "/" + comm.get(index).getRank() + ".png");
+			flop.getChildren().add(new ImageView(cardImage));
+		}
+		
+		community.getChildren().addAll(flop, streets);
+		
+		StackPane table = generateTable();
+		StackPane playerInfo = seatPlayers(players);
+		table.getChildren().addAll(community, playerInfo);
+		
+		root.setBottom(actionBar);
 		root.setCenter(table);
-		//root.setLeft(comBar);
 		primaryStage.show();
+	}
+	
+	/**
+	 * The geometry of the image of the poker table is formed from the
+	 * defined table size constants, returning such to the caller as a
+	 * StackPane.
+	 * 
+	 * @param none.
+	 * @return the nodes making up the table
+	 */
+	private static StackPane generateTable() {
+		Rectangle tableRectRim = new Rectangle(tableWidth, (tableWidth / tableRatio) + (tableRim * 2));
+		tableRectRim.setFill(Color.BLACK);
+		
+		Ellipse tableLeftRim = new Ellipse(((tableWidth / tableRatio) / 2) + tableRim, ((tableWidth / tableRatio) / 2) + tableRim);
+		tableLeftRim.setFill(Color.BLACK);
+		
+		Ellipse tableRightRim = new Ellipse(((tableWidth / tableRatio) / 2) + tableRim, ((tableWidth / tableRatio) / 2) + tableRim);
+		tableRightRim.setFill(Color.BLACK);
+		
+		Rectangle tableRect = new Rectangle(tableWidth, tableWidth / tableRatio);
+		tableRect.setFill(Color.FORESTGREEN);
+		
+		Ellipse tableLeft = new Ellipse((tableWidth / tableRatio) / 2, (tableWidth / tableRatio) / 2);
+		tableLeft.setFill(Color.FORESTGREEN);
+		
+		Ellipse tableRight = new Ellipse((tableWidth / tableRatio) / 2, (tableWidth / tableRatio) / 2);
+		tableRight.setFill(Color.FORESTGREEN);
+		
+		HBox lobesRims = new HBox();
+		lobesRims.setAlignment(Pos.CENTER);
+		lobesRims.setSpacing(tableWidth - (tableWidth / tableRatio) - (tableRim * 2));
+		lobesRims.getChildren().addAll(tableLeftRim, tableRightRim);
+		
+		HBox lobes = new HBox();
+		lobes.setAlignment(Pos.CENTER);
+		lobes.setSpacing(tableWidth - (tableWidth / tableRatio));
+		lobes.getChildren().addAll(tableLeft, tableRight);
+		
+		StackPane table = new StackPane();
+		table.setAlignment(Pos.CENTER);
+		table.getChildren().addAll(tableRectRim, lobesRims, tableRect, lobes);
+		
+		return table;
+	}
+	
+	private static StackPane seatPlayers(ArrayList<Player> players) {
+		double tableBorder = (tableWidth * 2) + (Math.PI * (tableWidth / tableRatio));
+		double playerSpacing = tableBorder / (double) players.size();
+		double bottomEnd = tableWidth / 2;
+		double leftEnd = bottomEnd + Math.PI * ((tableWidth / tableRatio) / 2);
+		double topEnd = leftEnd + tableWidth;
+		double rightEnd = topEnd + Math.PI * ((tableWidth / tableRatio) / 2);
+		
+		CircularPane leftSeats = new CircularPane();
+		CircularPane rightSeats = new CircularPane();
+		leftSeats.setDiameter((tableWidth / tableRatio) + (playerOutset * 2));
+		rightSeats.setDiameter((tableWidth / tableRatio) + (playerOutset * 2));
+		HBox bottomSeats = new HBox();
+		bottomSeats.setAlignment(Pos.CENTER);
+		HBox topSeats = new HBox();
+		topSeats.setAlignment(Pos.CENTER);
+		
+		VBox sWaySeats = new VBox();
+		sWaySeats.setAlignment(Pos.CENTER);
+		sWaySeats.setSpacing((tableWidth / tableRatio) + playerOutset);
+		sWaySeats.getChildren().addAll(topSeats, bottomSeats);
+		
+		HBox lobeSeats = new HBox();
+		lobeSeats.setAlignment(Pos.CENTER);
+		lobeSeats.setSpacing(tableWidth + (tableWidth / tableRatio) + playerOutset);
+		lobeSeats.getChildren().addAll(leftSeats, rightSeats);
+		
+		int currentPlayer = 0;
+		for (Player player : players) {
+			double distanceFromUser = currentPlayer * playerSpacing;
+			
+			Label name = new Label(player.getName());
+			Text stack = new Text("Stack: ");
+			Text action = new Text("Action: ");
+			
+			VBox seat = new VBox();
+			seat.setAlignment(Pos.CENTER);
+			seat.getChildren().addAll(name, stack, action);
+			
+			if (distanceFromUser < bottomEnd) {
+				bottomSeats.getChildren().add(seat);
+			}
+			else if (distanceFromUser < leftEnd) {
+				leftSeats.add(seat);
+			}
+			else if (distanceFromUser < topEnd) {
+				topSeats.getChildren().add(seat);
+			}
+			else if (distanceFromUser < rightEnd){
+				rightSeats.add(seat);
+			}
+			else {
+				bottomSeats.getChildren().add(seat);
+			}
+			currentPlayer++;
+		}
+		
+		bottomSeats.setSpacing(playerSpacing);
+		topSeats.setSpacing(playerSpacing);
+		leftSeats.setStartAngle(-135.0);
+		leftSeats.setArc((playerSpacing / (leftSeats.getDiameter() / 2.0)) * (180.0 / Math.PI));
+		rightSeats.setStartAngle(45.0);
+		rightSeats.setArc((playerSpacing / (leftSeats.getDiameter() / 2.0)) * (180.0 / Math.PI));
+		
+		StackPane playerInfo = new StackPane();
+		playerInfo.getChildren().addAll(lobeSeats, sWaySeats);
+		return playerInfo;
 	}
 	
 	public static void main(String[] args) {
 		launch(args);
-	}
-	
-	public static void passArrayLists(ArrayList<Player> playersDesig, ArrayList<Card> commDesig) {
-		players = playersDesig;
-		comm = commDesig;
-	}
-	
-	public static void updatePot(int newPot) {
-		pot = newPot;
-	}
-	
-	public static void updateHighestBet(int newHighestBet) {
-		highestBet = newHighestBet;
 	}
 }
