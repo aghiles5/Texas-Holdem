527ecc1c2697ac856461430688ec4002abf08220
aghiles5 <47369723+aghiles5@users.noreply.github.com>
2019-03-13 14:16:46 -0600

    Showdown method added to Game

--- Game.java
+++ Game.java
@@ -116,6 +116,28 @@ public class Game {
             return false;
         }
     }
+    
+    public ArrayList<Player> showdown() {
+    	ArrayList<Player> encapPlayers = new ArrayList<Player>(), winners = new ArrayList<Player>();
+		encapPlayers.addAll(roundPlayers); //The player ArrayList is encapsulated by the proxy encapPlayers ArrayList
+		
+		Player highestPlayer = encapPlayers.get(0);
+		int highPlayerIndex = -1;
+		for  (int index = 1; index < encapPlayers.size(); index++){
+			if (encapPlayers.get(index).getHand().compareHand(highestPlayer.getHand()) == 1)
+				highestPlayer = encapPlayers.get(index);
+				highPlayerIndex = index;
+		}
+		winners.add(highestPlayer);
+		encapPlayers.remove(highPlayerIndex);
+		
+		for (Player player : encapPlayers) {
+			if (player.getHand().compareHand(highestPlayer.getHand()) == 0)
+				winners.add(player);
+		}
+		
+		return winners;
+    }
 
     public void fold() {
         players.get(playerCount).fold("F");
