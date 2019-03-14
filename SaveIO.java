d556ed1f3552b20ca90fac5b65882b9bc8a6881d
Kyle Wen <Kyle Wen@10.13.152.90>
2019-03-14 10:26:28 -0600

    Update

--- SaveIO.java
+++ SaveIO.java
@@ -1,4 +1,16 @@
+import java.io.*;
 
 public class SaveIO {
-
+	public void SaveState(){
+		File dataFile = new File("Save1.txt");
+		FileWriter out;
+		BufferedWriter writeScore;
+		
+		try {
+			
+		} catch (IOException e) {
+			System.out.println("File not saved!");
+			System.err.println("IOException: " + e.getMessage());
+		}
+	}
 }
