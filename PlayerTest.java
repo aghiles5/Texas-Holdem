c92d75dea84ca81b2e31609aed33f3084928746a
John Lowie <jzlowie@yahoo.com>
2019-03-15 14:29:15 -0600

    Update PlayerTest.java

--- PlayerTest.java
+++ PlayerTest.java
@@ -1,3 +1,14 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
 import java.util.*;
+
+/**
+ * This is a test file for the Player class and its child classes, AI class and Human class
+ * 
+ * @author John Lowie
+ * @version 03/15/19
+ */
+
+public class PlayerTest {
+
+}
