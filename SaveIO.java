afcff683b994d4be4bd40305f786767abe640bfd
Kyle Wen <Kyle Wen@10.13.140.100>
2019-03-15 08:45:12 -0600

    finished save state with 1 error

--- SaveIO.java
+++ SaveIO.java
@@ -4,32 +4,47 @@ import java.util.ArrayList;
 /**
  * Saves All names and their stacks in a file.
  * @author Kyle Wen
- *
+ * @version March 15, 2019
  */
 public class SaveIO extends Game {
+	/**
+	 * pre: none
+	 * post: The players' names and their stack values are saved to a .txt file.
+	 */
 	public void SaveState(){
+		/*Copies the player list to a new ArrayList*/
 		ArrayList<Player> copy = new ArrayList<Player>(super.getPlayerList());
+		/*Two separate lists contain the name and stack of each player*/
 		ArrayList<String> name = new ArrayList<String>();
 		ArrayList<Integer> stacks = new ArrayList<Integer>();
-		File dataFile = new File("Save1.txt");
+		int saves = 1;
+		//writes to a new file each save
+		File dataFile = new File("Save" + saves + ".txt");
+		//how to load?
 		FileWriter out;
 		BufferedWriter writeScore;
 		
+		//adds the name and stack of each player to a separate list
 		for (int k = 0; k < copy.size(); k++) {
 			name.add(copy.get(k).getName());
 			stacks.add(copy.get(k).getStack());
 		}
 		
+		//writes to file
 		try {
-			out = new FileWriter(dataFile);
+			out = new FileWriter(dataFile); //output stream
 			writeScore = new BufferedWriter(out);
 			for (int i = 0; i < super.getPlayerList().size(); i++) {
-				//doesn't write with separate lines just one jumbled mess
 				writeScore.write(name.get(i));
+				writeScore.newLine();
 				writeScore.write(stacks.get(i));
+				writeScore.newLine();
 			}
+			writeScore.close();
+			out.close();
+			System.out.println("Game Saved.");
 		} catch (IOException e) {
-			System.out.println("File not saved!");
+			System.out.println("Game not saved!");
 			System.err.println("IOException: " + e.getMessage());
 		}
 	}
