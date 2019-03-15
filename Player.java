803c98c143476e24b537a80cc3a98b5b5b201ff1
John Lowie <jzlowie@yahoo.com>
2019-03-15 12:21:56 -0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -10,7 +10,7 @@ import java.util.*;
  * will also utilize these methods.
  * 
  * @author Kyle Wen, Adam Hiles, John Lowie
- * @version 03/13/2019
+ * @version 03/15/2019
  *
  */
 public abstract class Player {
