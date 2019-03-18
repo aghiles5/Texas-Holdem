5b03a8e302edffa6721e5df2fbff6da0733ad0ff
John Lowie <jzlowie@yahoo.com>
2019-03-18 11:13:47 -0600

    Update PlayerTest.java

--- PlayerTest.java
+++ PlayerTest.java
@@ -6,8 +6,11 @@ import java.util.*;
  * This is a test file for the Player class and its child classes, AI class and
  * Human class
  * 
+ * JUnit testing will not be used as there were unresolved technical difficulties
+ * importing JUnit test packages
+ * 
  * @author John Lowie
- * @version 03/15/19
+ * @version 03/18/19
  */
 
  public class PlayerTest {
