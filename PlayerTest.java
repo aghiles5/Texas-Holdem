fcc2025f04e28773d7c5faebb8ed1ee07466bc2d
John Lowie <jzlowie@yahoo.com>
2019-03-18 14:30:38 -0600

    Update PlayerTest.java

--- PlayerTest.java
+++ PlayerTest.java
@@ -16,26 +16,7 @@
     }
 
     public static void testing() {
-        AI a = new AI();
-        AI b = new AI();
-        AI c = new AI();
-        AI d = new AI();
-        AI e = new AI();
-        AI f = new AI();
-        AI g = new AI();
-        AI h = new AI();
-        AI i = new AI();
-        AI j = new AI();
-        System.out.println(a.getCPUName());
-        System.out.println(b.getCPUName());
-        System.out.println(c.getCPUName());
-        System.out.println(d.getCPUName());
-        System.out.println(e.getCPUName());
-        System.out.println(f.getCPUName());
-        System.out.println(g.getCPUName());
-        System.out.println(h.getCPUName());
-        System.out.println(i.getCPUName());
-        System.out.println(j.getCPUName());
+        
     }
 
  }
