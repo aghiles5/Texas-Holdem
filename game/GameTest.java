37c39bfbbde06563386c97a040a05001de24fa73
BSchmaltzC <47369750+BSchmaltzC@users.noreply.github.com>
2019-03-27 23:31:02 -0600

    Add files via upload

---
+++ game/GameTest.java
@@ -0,0 +1,13 @@
+package game;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class GameTest {
+	// testing game constructor
+	@Test
+	public void test_GameConstructor() {
+		Game game = new Game();
+
+	}
+}
