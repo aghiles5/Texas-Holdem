fe58c5a29b66dcfb221386a56cc2c101405cb55c
aghiles5 <47369723+aghiles5@users.noreply.github.com>
2019-04-06 16:51:16 -0600

    Music fully functional

--- gui/MusicPlayer.java
+++ gui/MusicPlayer.java
@@ -4,29 +4,38 @@ import java.util.Random;
 import java.net.URL;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 
 public class MusicPlayer {
-	private final String[] filenames = {"/gunfighterBallads/01.mp3", "/gunfighterBallads/02.mp3", "/gunfighterBallads/09.mp3"};
+	private final String[] filenames = {"/gunfighterBallads/01.mp3", "/gunfighterBallads/02.mp3", "/gunfighterBallads/03.mp3",
+			"/gunfighterBallads/04.mp3", "/gunfighterBallads/05.mp3", "/gunfighterBallads/06.mp3", "/gunfighterBallads/07.mp3",
+			"/gunfighterBallads/08.mp3", "/gunfighterBallads/09.mp3", "/gunfighterBallads/11.mp3", "/gunfighterBallads/12.mp3",
+			"/gunfighterBallads/13.mp3", "/gunfighterBallads/14.mp3", "/gunfighterBallads/15.mp3"};
 	private int prevIndex = -1;
+	private MediaView view = new MediaView();
 	
 	public void play() {
-		int index = -1;
+		int index = prevIndex;
 		Random rand = new Random();
 		while (index == prevIndex) {
 			index = rand.nextInt(filenames.length);
 		}
 		
 		URL resource = getClass().getResource(filenames[index]);
+		System.out.println(prevIndex + " " + index + " " + filenames[index]);
 		Media medium = new Media(resource.toString());
 		MediaPlayer player = new MediaPlayer(medium);
+		view.setMediaPlayer(player);
+		
+		prevIndex = index;
+		player.play();
 		
 		player.setOnEndOfMedia(new Runnable() {
 		    @Override
 		    public void run() {
+		    	player.stop();
 		        play();
 		    }
 		});
-		
-		player.play();
 	}
 }
